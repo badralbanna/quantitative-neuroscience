9329884e6722323641d29daf92f374e00f946e8b
Badr Albanna <badr.albanna@gmail.com>
2020-09-16 22:26:58 -0400

    Modified script

--- install_qn_kernel.sh
+++ install_qn_kernel.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 module purge
-module load python/3.7.0 
-module load venv/wrap
+module load python/3.7.0 venv/wrap
 mkvirtualenv -p python py3-qn
 workon py3-qn
 pip install ipykernel
