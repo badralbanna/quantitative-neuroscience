b2789b1de0e94a54660f738725682e740647c07d
Badr Albanna <badr.albanna@gmail.com>
2020-10-16 00:43:50 -0400

    Modified the install kernel

--- install_qn_kernel.sh
+++ install_qn_kernel.sh
@@ -4,5 +4,5 @@ module purge
 module load python/3.7.0 venv/wrap
 mkvirtualenv -p python py3-qn
 workon py3-qn
-pip install ipykernel
+pip install ipykernel numpy scipy matplotlib xarray pandas h5py dowhy causalgraphicalmodels
 python -m ipykernel install --user --name=py3-qn --display-name "Python 3 (QN)"
