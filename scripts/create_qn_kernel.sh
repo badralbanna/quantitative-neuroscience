b605bde4e4f963b0723c85a1fb24cb12b1cd2ba6
Badr Albanna <badr.albanna@gmail.com>
2020-10-23 17:22:59 -0400

    Caught a bug

--- scripts/create_qn_kernel.sh
+++ scripts/create_qn_kernel.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-./create_kernel.sh py3-qn "Python 3 (QN)" numpy scipy matplotlib xarray pandas h5py dowhy causalgraphicalmodels seaborn:w
-:q
+./create_kernel.sh py3-qn "Python 3 (QN)" numpy scipy matplotlib xarray pandas h5py dowhy causalgraphicalmodels seaborn statsmodels
 
