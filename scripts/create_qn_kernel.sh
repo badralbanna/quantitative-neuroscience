a5641563a196818f9fa2d1a9c66465affc1c7fe4
Badr Albanna <badr.albanna@gmail.com>
2020-10-28 05:17:57 -0400

    Updated kernel packages

--- scripts/create_qn_kernel.sh
+++ scripts/create_qn_kernel.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-./create_kernel.sh py3-qn "Python 3 (QN)" numpy scipy matplotlib xarray pandas h5py dowhy causalgraphicalmodels seaborn statsmodels
+./create_kernel.sh py3-qn "Python 3 (QN)" numpy scipy matplotlib xarray pandas h5py patsy graphicx dowhy causalgraphicalmodels seaborn statsmodels scikit-learn dabest
 
