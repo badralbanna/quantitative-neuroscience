79d76be171d64f79104f86cdcbfbf3dbcccb4741
Badr Albanna <badr.albanna@gmail.com>
2021-09-09 11:35:38 -0400

    Updated script

--- scripts/create_qn_kernel.sh
+++ scripts/create_qn_kernel.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-/ihome/sam/bmooreii/workspace/crc-wrappers/jupyter-kernel/crc-jupyter-kernel.py -n py3-qn -d "Python 3 (QN)" -p "numpy scipy matplotlib seaborn xarray pandas xlrd h5py patsy dowhy causalgraphicalmodels seaborn statsmodels scikit-learn dabest"
+module load python/3.7.0
+python /ihome/sam/bmooreii/workspace/crc-wrappers/jupyter-kernel/crc-jupyter-kernel.py -n py3-qn -d "Python 3 (QN)" -p "numpy scipy matplotlib seaborn xarray pandas xlrd h5py patsy dowhy causalgraphicalmodels seaborn statsmodels scikit-learn dabest"
 
