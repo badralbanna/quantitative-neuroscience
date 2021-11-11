bf12eda56855d930468c988b4404d6859959285a
Badr Albanna <badr.albanna@gmail.com>
2021-11-11 15:47:25 -0500

    HW 4 updates

--- HW 4 data generator.py
+++ HW 4 data generator.py
@@ -3,4 +3,11 @@ import os
 import numpy as np
 import pickle as pickle 
 
-# Problem 
+# Problem 4.1
+Y1 = np.random.randn(loc=0, scale=2, size=50)
+Y2 = np.random.randn(loc=0.3, scale=2, size=100)
+
+with file("P4-1_data.npy", "wb") as f:
+    Y1.save(f)
+    Y2.save(f)
+
