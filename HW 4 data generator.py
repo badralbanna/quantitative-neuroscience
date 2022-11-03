ebe75c32d016bb42991e016e9d030546ff07ebcf
badralbanna <badr.albanna@gmail.com>
2022-11-03 15:21:33 -0400

    Updated HW3 and HW4

--- HW 4 data generator.py
+++ HW 4 data generator.py
@@ -4,10 +4,20 @@ import numpy as np
 import pickle as pickle 
 
 # Problem 4.1
-Y1 = np.random.randn(loc=0, scale=2, size=50)
-Y2 = np.random.randn(loc=0.3, scale=2, size=100)
+Y1 = np.random.randn(83)*2
+Y2 = np.random.randn(94)*2 + 0.3
 
-with file("P4-1_data.npy", "wb") as f:
-    Y1.save(f)
-    Y2.save(f)
+with open(os.path.join("data", "P4-1-Y1_data.npy"), "wb") as f:
+    np.save(f, Y1)
+with open(os.path.join("data", "P4-1-Y2_data.npy"), "wb") as f:
+    np.save(f, Y2)
+
+# Problem 4.2 
+Z1 = (np.random.randn(94)*2.3)**5
+Z2 = (np.random.randn(79)*2.3 + 0.65)**3
+
+with open(os.path.join("data", "P4-2-Z1_data.npy"), "wb") as f:
+    np.save(f, Z1)
+with open(os.path.join("data", "P4-2-Z2_data.npy"), "wb") as f:
+    np.save(f, Z2)
 
